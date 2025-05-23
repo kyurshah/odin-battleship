261a2dcb524d81442ef06a5635630f95761d0386
keyur <13107634+kyurshah@users.noreply.github.com>
2025-05-23 18:51:59 +0530

    add initial test

---
+++ battleship.test.js
@@ -0,0 +1,5 @@
+const ship = require('./ship');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(ship(1, 2)).toBe(3);
+});
---
+++ ship.js
@@ -0,0 +1,5 @@
+function ship(a, b) {
+    return a + b;
+  }
+  module.exports = ship;
+  
